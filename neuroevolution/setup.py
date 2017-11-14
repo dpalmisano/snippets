640486021ce16cfbde434c1d354909bc61b741a9
dpalmisano <dpalmisano@gmail.com>
2017-11-14 10:49:45 +0000

    Bump version up.

--- neuroevolution/setup.py
+++ neuroevolution/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='neuroevolution',
-      version='0.1.4',
+      version='0.1.5',
       description='Neuroevolution algorithms for your Keras model.',
       url='https://github.com/dpalmisano/snippets',
       author='Davide Palmisano',
