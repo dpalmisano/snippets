872904fbd44a5e6fcd46af2127d68e4a5803033a
dpalmisano <dpalmisano@gmail.com>
2017-11-15 12:16:18 +0000

    Refine distribution script.

--- neuroevolution/dist.sh
+++ neuroevolution/dist.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-python setup.py sdist
+echo 'Removing previous distributions...'
+rm -r mah 2> /dev/null
+echo 'Packaging...'
+python setup.py sdist > /dev/null
+dist_file=$(ls dist)
+echo 'Uploading'
+curl -F package=@dist/$dist_file https://PoFnWtvqSTuH2U4FG1wX@push.fury.io/dpalmisano/
 
