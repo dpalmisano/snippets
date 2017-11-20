95a7a580460e674565573e2d79a1ee6f5d25da49
dpalmisano <dpalmisano@gmail.com>
2017-11-20 10:41:28 +0000

    Neuroevolution moved to its own repo.

--- neuroevolution/neuroevolution/utils.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def random_fitting(start, end, size, fitting_function, err_interval, error = 'normal', ):
-    x = np.linspace(start, end, size)
-    delta = getattr(np.random, error)(-err_interval, err_interval, x.size)
-    return (x, fitting_function(x) + delta)
